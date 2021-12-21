32ac093e579461bba1fe0f69935d0eecea8d9abc
adi wirya <adi.wirya@student.umn.ac.id>
2021-12-21 15:40:57 +0700

    paket

--- lib/screens/paket_detail_screen.dart
+++ lib/screens/paket_detail_screen.dart
@@ -6,6 +6,54 @@ class PaketDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                'https://images.weddingku.com/images/upload/store/product/big/638-hlco2sree8g0.jpg',
+              ),
+            ),
+           //start
+          Container(
+              height: 150,
+              child: Card(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Image.network(
+                        'https://picsum.photos/250?image=9',
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 50,
+                            child: Center(child: Text('abc')),
+                          ),
+                          Expanded(flex: 25, child: Text('def')),
+                          Expanded(flex: 25, child: Text('ghi')),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+           //end
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/paket_overview_screen.dart
+++ lib/screens/paket_overview_screen.dart
@@ -4,69 +4,82 @@ import 'package:dio/dio.dart';
 class PaketOverviewScreen extends StatelessWidget {
   const PaketOverviewScreen({Key? key}) : super(key: key);
   static const routeName = '/overview';
-
   @override
   Widget build(BuildContext context) {
+      var screensize = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         body: ListView(
           children: [
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/detail');
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+            card_item(),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class card_item extends StatelessWidget {
+  const card_item({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail');
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
                 ),
-                elevation: 4,
-                margin: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                        child: Image.network(
-                          'https://images.weddingku.com/images/upload/store/product/big/638-hlco2sree8g0.jpg',
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: 250,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Flexible(
-                            flex: 50,
-                            child: Expanded(
-                              child: Column(
-                                children: [
-                                  Text(
-                                    'Paket 1',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Rp.1.000.000',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                child: Image.network(
+                  'https://images.weddingku.com/images/upload/store/product/big/638-hlco2sree8g0.jpg',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 250,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1 Paket 1',
+                          style: TextStyle(
+                            fontSize: 20,
                           ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'Rp.1.000.000',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
